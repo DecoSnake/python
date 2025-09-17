943ba24ba2bd8ed54a11fd839ef9c02b36101fcf
DecoSnake <110052365+DecoSnake@users.noreply.github.com>
2025-09-17 14:13:53 -0300

    gisele.py

--- gisele.py
+++ gisele.py
@@ -1,3 +1,26 @@
+python
+import streamlit as st
+import base64
+
+def set_bg_image(image_path):
+    with open(image_path, "rb") as img:
+        encoded = base64.b64encode(img.read()).decode()
+    css = f"""
+    <style>
+    .stApp {{
+        background-image: url("data:image/jpg;base64,{encoded}");
+        background-size: cover;
+        background-position: center;
+        background-repeat: no-repeat;
+    }}
+    </style>
+    """
+    st.markdown(css, unsafe_allow_html=True)
+
+Ativa o fundo
+set_bg_image("fundo.jpg")
+
+
 import streamlit as st
 import pandas as pd
 from datetime import datetime
